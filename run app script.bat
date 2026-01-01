c2a3d952089b59d438b4430ac0ff34c4b92de385
segevstat <81333605+segevstat@users.noreply.github.com>
2026-01-01 14:29:47 +0200

    Add files via upload

---
+++ run app script.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Step 1: Navigating to project directory...
+cd /d C:\Users\segev\code_notebooks\HTs\IAI_ELT
+
+echo Step 2: Starting Model Training...
+python train.py
+
+echo Step 3: Launching Streamlit App...
+streamlit run ds_app.py
+
+pause
