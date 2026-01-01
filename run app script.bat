fd588d8b6838b2a78d05ad6b53fe7129d54c52a6
segevstat <81333605+segevstat@users.noreply.github.com>
2026-01-01 14:43:08 +0200

    Update run app script.bat

--- run app script.bat
+++ run app script.bat
@@ -1,11 +1,12 @@
 @echo off
 echo Step 1: Navigating to project directory...
-cd /d C:\Users\segev\code_notebooks\HTs\IAI_ELT
+cd /d C:\Users\yourpath\
 
 echo Step 2: Starting Model Training...
 python train.py
 
 echo Step 3: Launching Streamlit App...
 streamlit run ds_app.py
+
 
 pause
